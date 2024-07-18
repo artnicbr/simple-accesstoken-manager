b1bdb083fbb2c454a82b493009f17dda80ac9c6e
André \ricardo Cintra <andre.cintra@unimedsaocarlos.com.br>
2024-07-18 14:13:21 -0300

    Some fixes

--- satm/myapi/classes/User.py
+++ satm/myapi/classes/User.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
+from django.core.serializers import serialize
 from rest_framework.views import APIView
 from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
 import datetime
 from rest_framework.response import Response
 import hashlib
+import json
 
 from ..models import User
 from ..models import Token
 from .Log import LogRecord
 
 class UserView(APIView):
+    def get(self, request, format="json"):
+        resp = None
+        try:
+            resp = {"data": json.loads(serialize("json", User.objects.all(), fields=("LOGIN","EMAIL", "TIMESTAMP_INS", "TIMESTAMP_UPD")))}
+                             
+        except Exception as ex:
+            LogRecord.save("Login", request.data, resp)
+            resp = {
+                "status": False,
+                "code": "U01-026",
+                "message": str(ex)
+            }
+            print(str(ex))
+
+        return Response(resp)
+
     def post(self, request, format=None):
         try:
             user = User.objects.filter(
--- satm/myapi/urls.py
+++ satm/myapi/urls.py
@@ -7,6 +7,7 @@ from .views import AdminView
 urlpatterns = [
     re_path(r'^version/$', VersionView.as_view()),
     re_path(r'^auth/$', UserView.as_view()),
+    re_path(r'^list/$', UserView.as_view()),
     re_path(r'^validate/$', TokenView.as_view()),
     re_path(r'^register/$', AdminView.as_view()),
 ]
